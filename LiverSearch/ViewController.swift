33ee68565c67692c94020f0c5fc7b04641dd0b8d
Axel Iván Solano González <pandaxel117@gmail.com>
2021-07-05 11:06:46 -0500

    Initial Commit

---
+++ LiverSearch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiverSearch
+//
+//  Created by Axel Iván Solano González on 05/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
